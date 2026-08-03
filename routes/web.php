e89f70713dc0aad1fb4fca270d6cb779be8a00ff
AdnanKhan9237 <adnanwahid9237@gmail.com>
2026-08-03 13:55:37 +0500

    4

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/up', function () {
-    return response()->json(['status' => 'ok']);
-});
-
 Route::get('/login', function () {
     return response()->json(['message' => 'Unauthenticated.'], 401);
 })->name('login');
